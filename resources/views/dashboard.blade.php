796566a4f40fd4a9da4172836d39cad0c43c5918
AJ Meireles <alvaro.meireles@live.com>
2025-03-26 20:19:21 -0300

    updating dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,21 +7,27 @@
             <p>
                 👋🏻 This is the TallStackUI starter kit for Laravel 12. With this TallStackUI starter kit you will be able to enjoy a ready-to-use application to initialize your next Laravel 12 project with TallStackUI.
             </p>
-            <div>
+            <div class="mt-4 space-y-2">
                 <i>
-                    "What this starter kit includes?"
+                    "What this starter kit includes out of the box?"
                 </i>
-                <ul class="ml-2 mt-2 list-inside list-decimal font-bold">
+                <ul class="ml-2 mt-2 list-inside list-decimal font-semibold">
                     <li>Laravel v12</li>
                     <li>Livewire v3</li>
                     <li>TallStackUI v2</li>
                     <li>TailwindCSS v4</li>
                 </ul>
+                <p>And also:</p>
+                <ul class="ml-2 mt-2 list-inside list-decimal font-semibold">
+                    <li>Pest</li>
+                    <li>LaraStan</li>
+                    <li>Pint</li>
+                </ul>
             </div>
         </div>
         <x-slot:footer>
             <span class="text-xs">
-                ✨ We've prepared a basic <i>C.R.U.D.</i> for users in the app/Livewire/Users folder using several TallStackUI components. <x-link :href="route('users.index')" colorless underline>Click here to try it out!</x-link>
+                ⚠️ <x-link href="https://tallstackui.com/docs/v2/starter-kit" bold blank sm>Make sure to read the docs about the starter kit!</x-link>
             </span>
         </x-slot:footer>
     </x-card>
