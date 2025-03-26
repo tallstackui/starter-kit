a33308e3229b3b5e5241755c2ce2d21a10981026
AJ Meireles <alvaro.meireles@live.com>
2025-03-26 14:14:16 -0300

    dark theme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-data="tallstackui_darkTheme()">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,11 +14,15 @@
         @livewireStyles
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <body class="font-sans antialiased" x-cloak x-bind:class="{ 'dark bg-gray-700': darkTheme, 'bg-gray-100': !darkTheme }">
+        <div class="min-h-screen">
             <x-layout>
                 <x-slot:header>
                     <x-layout.header>
+                        <x-slot:left>
+                            <x-theme-switch />
+                        </x-slot:left>
+
                         <x-slot:right>
                             <x-dropdown text="Hello, {{ auth()->user()->name }}!">
                                 <form method="POST" action="{{ route('logout') }}">
@@ -32,7 +36,14 @@
 
                 <x-slot:menu>
                     <x-side-bar smart>
+                        <x-slot:brand>
+                            <div class="flex justify-center items-center mt-6">
+                                <x-app-logo class="size-14 text-[#f53003]" />
+                            </div>
+                        </x-slot:brand>
                         <x-side-bar.item text="Dashboard" icon="home" :route="route('dashboard')" />
+                        <x-side-bar.item text="Users" icon="users" :route="route('dashboard')" />
+                        <x-side-bar.item text="Welcome Page" icon="arrow-uturn-left" :route="route('welcome')" />
                     </x-side-bar>
                 </x-slot:menu>
 
