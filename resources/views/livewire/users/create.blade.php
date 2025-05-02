9815edd2d6963ce3bd9f0dfc5f274ed65e17d683
Bruno Costa <bhcosta90@gmail.com>
2025-05-02 09:32:02 -0300

    localizing user creation form labels and buttons in create.blade.php

--- resources/views/livewire/users/create.blade.php
+++ resources/views/livewire/users/create.blade.php
@@ -1,18 +1,18 @@
 <div>
-    <x-button text="Create New User" wire:click="$toggle('modal')" sm />
+    <x-button :text="__('Create New User')" wire:click="$toggle('modal')" sm />
 
     <x-modal :title="__('Create New User')" wire x-on:open="setTimeout(() => $refs.name.focus(), 250)">
         <form id="user-create" wire:submit="save" class="space-y-4">
             <div>
-                <x-input label="Name *" x-ref="name" wire:model="user.name" required />
+                <x-input label="{{ __('Name') }} *" x-ref="name" wire:model="user.name" required />
             </div>
 
             <div>
-                <x-input label="Email *" wire:model="user.email" required />
+                <x-input label="{{ __('Email') }} *" wire:model="user.email" required />
             </div>
 
             <div>
-                <x-password label="Password *"
+                <x-password label="{{ __('Password') }} *"
                             wire:model="password"
                             rules
                             generator
@@ -21,12 +21,12 @@
             </div>
 
             <div>
-                <x-password label="Password" wire:model="password_confirmation" rules required />
+                <x-password :label="__('Password')" wire:model="password_confirmation" rules required />
             </div>
         </form>
         <x-slot:footer>
             <x-button type="submit" form="user-create">
-                Save
+                @lang('Save')
             </x-button>
         </x-slot:footer>
     </x-modal>
