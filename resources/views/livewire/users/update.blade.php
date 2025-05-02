83d608c75cad20792cf869d3694fd0851f98a733
Bruno Costa <bhcosta90@gmail.com>
2025-05-02 09:32:18 -0300

    localizing labels and buttons in update.blade.php

--- resources/views/livewire/users/update.blade.php
+++ resources/views/livewire/users/update.blade.php
@@ -2,15 +2,15 @@
     <x-modal :title="__('Update User: #:id', ['id' => $user?->id])" wire>
         <form id="user-update-{{ $user?->id }}" wire:submit="save" class="space-y-4">
             <div>
-                <x-input label="Name *" wire:model="user.name" required />
+                <x-input label="{{ __('Name') }} *" wire:model="user.name" required />
             </div>
 
             <div>
-                <x-input label="Email *" wire:model="user.email" required />
+                <x-input label="{{ __('Email') }} *" wire:model="user.email" required />
             </div>
 
             <div>
-                <x-password label="Password"
+                <x-password :label="__('Password')"
                             hint="The password will only be updated if you set the value of this field"
                             wire:model="password"
                             rules
@@ -19,12 +19,12 @@
             </div>
 
             <div>
-                <x-password label="Password" wire:model="password_confirmation" rules />
+                <x-password :label="__('Password')" wire:model="password_confirmation" rules />
             </div>
         </form>
         <x-slot:footer>
             <x-button type="submit" form="user-update-{{ $user?->id }}">
-                Save
+                @lang('Save')
             </x-button>
         </x-slot:footer>
     </x-modal>
