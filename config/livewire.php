c8a25159d750276197484a084aecfca525817840
AJ Meireles <alvaro.meireles@live.com>
2026-08-14 16:11:04 -0300

    feat: default make command to class components without emoji

--- config/livewire.php
+++ config/livewire.php
@@ -71,8 +71,8 @@ return [
     */
 
     'make_command' => [
-        'type'  => 'sfc', // Options: 'sfc', 'mfc', 'class'
-        'emoji' => true, // Options: true, false
+        'type'  => 'class', // Options: 'sfc', 'mfc', 'class'
+        'emoji' => false, // Options: true, false
         'with'  => [
             'js'   => false,
             'css'  => false,
